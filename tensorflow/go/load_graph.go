b34f0f87cbd54bb48665048c07d6cea690c7efd5
Berner Setterwall <berner.setterwall@campanja.com>
2016-09-13 10:15:00 +0200

    Use protobuf datatypes

--- tensorflow/go/load_graph.go
+++ tensorflow/go/load_graph.go
@@ -1,52 +1,43 @@
 package tensorflow
 
+// #include "tensorflow/c/c_api.h"
+import "C"
+
 import (
 	"fmt"
+    pb "github.com/berset/tensorflow/tensorflow/go/pb/tensorflow/core/framework"
 )
 
-type Node struct {
-	Name   string
-	Op     string
-	Inputs []string
-	Attrs  []Attr
-}
-
-type Attr struct {
-	Key   string
-	Value interface{}
-}
-
-func loadNodes() []Node {
-	t1, _ := NewTensor(int64(1))
-	dt := t1.DataType()
-
-	var nodes []Node
-	nodes = append(nodes, Node{
+func loadNodes() []pb.NodeDef {
+	var nodes []pb.NodeDef
+	nodes = append(nodes, pb.NodeDef{
 		Name: "p1",
 		Op:   "Placeholder",
-		Attrs: []Attr{
-			Attr{
-				Key:   "dtype",
-				Value: dt,
-			},
+		Attr: map[string]*pb.AttrValue{
+            "dtype": &pb.AttrValue{
+                Value: &pb.AttrValue_Type{
+                    Type: pb.DataType_DT_INT64,
+			    },
+            },
 		},
 	})
 
-	nodes = append(nodes, Node{
+	nodes = append(nodes, pb.NodeDef{
 		Name: "p2",
 		Op:   "Placeholder",
-		Attrs: []Attr{
-			Attr{
-				Key:   "dtype",
-				Value: dt,
-			},
+		Attr: map[string]*pb.AttrValue{
+            "dtype": &pb.AttrValue{
+                Value: &pb.AttrValue_Type{
+                    Type: pb.DataType_DT_INT64,
+			    },
+            },
 		},
 	})
 
-	nodes = append(nodes, Node{
-		Name:   "neg1",
-		Op:     "Add",
-		Inputs: []string{"p1", "p2"},
+	nodes = append(nodes, pb.NodeDef{
+		Name:  "neg1",
+		Op:    "Add",
+		Input: []string{"p1", "p2"},
 	})
 
 	return nodes
@@ -63,12 +54,10 @@ func LoadGraph(graphFileName string) (*Graph, map[string]Output, error) {
 
 	for _, node := range nodes {
 		b := newOpBuilder(g, node.Op, node.Name)
-		for _, attr := range node.Attrs {
-			if attr.Key == "dtype" {
-				b.SetAttrType("dtype", attr.Value.(DataType))
-			}
+		for _, attr := range node.Attr {
+            handleAttr(b, attr)
 		}
-		for _, input := range node.Inputs {
+		for _, input := range node.Input {
 			b.AddInput(ns[input])
 		}
 		op, err := b.Build()
@@ -81,4 +70,54 @@ func LoadGraph(graphFileName string) (*Graph, map[string]Output, error) {
 	}
 
 	return g, ns, nil
+}
+
+func handleAttr(b *opBuilder, m *pb.AttrValue) error {
+	switch x := m.Value.(type) {
+//	case *AttrValue_S:
+//		b.EncodeVarint(2<<3 | proto.WireBytes)
+//		b.EncodeRawBytes(x.S)
+//	case *AttrValue_I:
+//		b.EncodeVarint(3<<3 | proto.WireVarint)
+//		b.EncodeVarint(uint64(x.I))
+//	case *AttrValue_F:
+//		b.EncodeVarint(4<<3 | proto.WireFixed32)
+//		b.EncodeFixed32(uint64(math.Float32bits(x.F)))
+//	case *AttrValue_B:
+//		t := uint64(0)
+//		if x.B {
+//			t = 1
+//		}
+//		b.EncodeVarint(5<<3 | proto.WireVarint)
+//		b.EncodeVarint(t)
+	case *pb.AttrValue_Type:
+		b.SetAttrType("dtype", DataType(m.GetType()))
+//	case *AttrValue_Shape:
+//		b.EncodeVarint(7<<3 | proto.WireBytes)
+//		if err := b.EncodeMessage(x.Shape); err != nil {
+//			return err
+//		}
+//	case *AttrValue_Tensor:
+//		b.EncodeVarint(8<<3 | proto.WireBytes)
+//		if err := b.EncodeMessage(x.Tensor); err != nil {
+//			return err
+//		}
+//	case *AttrValue_List:
+//		b.EncodeVarint(1<<3 | proto.WireBytes)
+//		if err := b.EncodeMessage(x.List); err != nil {
+//			return err
+//		}
+//	case *AttrValue_Func:
+//		b.EncodeVarint(10<<3 | proto.WireBytes)
+//		if err := b.EncodeMessage(x.Func); err != nil {
+//			return err
+//		}
+//	case *AttrValue_Placeholder:
+//		b.EncodeVarint(9<<3 | proto.WireBytes)
+//		b.EncodeStringBytes(x.Placeholder)
+	case nil:
+	default:
+		return fmt.Errorf("AttrValue.Value has unexpected type %T", x)
+	}
+	return nil
 }
